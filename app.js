93123fa4a91c0175deaeb7cae34e3a6ea7f767ee
dev-rfgul <rfgul587@gmail.com>
2024-09-18 21:50:21 +0500

    implemented video no 11 and completed the crud functions

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.get('/', (req, res) => {
 app.get('/create', async (req, res) => {
     let createdUser = await userModel.create({
         name: "Fahad",
-        username: "fahad12322",
+        username: "Fahad",
         email: "raofahadgul785@gmail.com"
     })
     try {
@@ -23,10 +23,30 @@ app.get('/create', async (req, res) => {
     }
 })
 
-app.get('/about', (req, res) => {
-    res.send('This is about page')
+app.get('/update', async (req, res) => {
+    // userModel.findOneAndUpdate(findone,updated,{new:true}) these are the parameters of the function
+    let updatedUser = await userModel.findOneAndUpdate({ name: "Fahad" }, { name: "Fahad Gul" }, { new: true }
+    )
+    res.send(updatedUser)
 })
 
+app.get('/read', async (req, res) => {
+    let users = await userModel.find();
+    //find will read all the users and if we want to read the data of one user we will pass it as a parameter in find function
+
+    let user = await userModel.find({ name: "Fahad" })
+
+    // there is a diff btw the find and fineOne as find will give you an array even if the user is not found it will give an empty array
+    // but findone will give you null if the user is not found and if found it will give and object, and it will give the first object it finds
+    res.send(user)
+    // res.send(users)
+
+})
+
+app.get('/delete', async (req, res) => {
+    let user= await userModel.findOneAndDelete({name:"Fahad"})
+    res.send(user)
+})
 
 app.get('/users', async (req, res) => {
     res.send("This is users page")
